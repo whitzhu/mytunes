05e28fa66e228aea0b9fec7c40c60f1f08b935c4
whitzhu <whitzhu@gmail.com>
2017-02-20 16:54:16 -0800

    Passing tests for SongQueueView

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,12 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
   // your code here!
+  initialize: function() {
+    this.render();
+  },
+
+  render: function() {
+    var html = `<div>${ this.model.get('title') } - ${ this.model.get('artist')}</div>`;
+    return this.$el.html(html);
+  }
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,10 +1,17 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  collection: SongQueue,
+
   initialize: function() {
+    this.render();
   },
 
   render: function() {
+    this.collection.map(function(song) {
+      var mySong = new SongQueueEntryView({ model: song });
+      mySong.render();
+    });
     return this.$el;
   }
 
